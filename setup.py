29e64d791868019aee58dd40c080f4f85191ca5a
Sam Raker <sam.raker@gmail.com>
2015-06-15 21:26:45 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='miniconfig',
-        version='0.0.1',
+        version='0.0.2',
         description='Minimal config in Python',
         url='https://github.com/swizzard/miniconfig',
         author='Sam Raker',
